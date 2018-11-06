59bb2a13b0c367ca0a31b033c6984b138af7b31c
DESKTOP-049UNM6\Malcolm <malferg90@gmail.com>
2018-11-06 15:34:01 -0500

    W & S now move the tank forward and backward!

--- BattleTank/Source/BattleTank/Private/TankMovementComponent.cpp
+++ BattleTank/Source/BattleTank/Private/TankMovementComponent.cpp
@@ -2,9 +2,21 @@
 
 #include "TankMovementComponent.h"
 #include "BattleTank.h"
+#include "TankTrack.h"
+
+void UTankMovementComponent::Initialize(UTankTrack* LeftTrackToSet, UTankTrack* RightTrackToSet)
+{
+	if (!LeftTrackToSet || !RightTrackToSet) { return; }
+	LeftTrack = LeftTrackToSet;
+	RightTrack = RightTrackToSet;
+}
 
 void UTankMovementComponent::IntendMoveForward(float Throw)
 {
 	UE_LOG(LogTemp, Warning, TEXT("Intend move forward throw: %f"), Throw);
+
+	LeftTrack->SetThrottle(Throw);
+	RightTrack->SetThrottle(Throw);
+	// TODO: Prevent double speed due to dual control use
 }
 
